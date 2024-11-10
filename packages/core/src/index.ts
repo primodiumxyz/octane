3bc0eadf6e5863361ea284e7796ab2b290c66a41
CometShock <cometshockcrypto@gmail.com>
2024-11-09 18:46:53 -0600

    fix: export types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 export * from './actions';
 export * as core from './core-functions';
+export type * from './core-functions';
 export * as PayerUtils from './payer-utils';
 export * from './swapProviders';
